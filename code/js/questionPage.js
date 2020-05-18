e4e8cd41a41fc7f82a89ce0747ef339e312ab4a0
姜敏 <jiangmin@asiainfo.com>
2020-05-18 17:17:51 +0800

    问卷预览

--- code/js/questionPage.js
+++ code/js/questionPage.js
@@ -72,10 +72,90 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
   }
   /******************************************************* 创建问卷 *******************************************************/
   //保存第二步数据
-  var step2Data = {
+  var questPageData = {
     questTitle: "",  // 问卷标题
     questList: []  // 问卷列表数据
   };
+  // //测试数据
+  // var questPageData = {
+  //   questTitle: "客户满意度调查",  // 问卷标题
+  //   questList: [{
+  //     itemType: 'radio', //类型
+  //     itemCode: '', //编码
+  //     itemDesc: '问题一', //名称
+  //     isRequired: true, //是否必填
+  //     options: [{ //描述
+  //       optionDes: '选项0',
+  //       optionId: '0'
+  //     }, {
+  //       optionDes: '选项1',
+  //       optionId: '1'
+  //     }, { //描述
+  //       optionDes: '选项2',
+  //       optionId: '2'
+  //     }, {
+  //       optionDes: '选项3',
+  //       optionId: '3'
+  //     }, { //描述
+  //       optionDes: '选项4',
+  //       optionId: '4'
+  //     }, {
+  //       optionDes: '选项5',
+  //       optionId: '5'
+  //     }],
+  //     itemGrade: '1', //级别
+  //   }, {
+  //     itemType: 'checkbox', //类型
+  //     itemCode: '', //编码
+  //     itemDesc: '问题二', //名称
+  //     isRequired: true, //是否必填
+  //     options: [{ //描述
+  //       optionDes: '选项0',
+  //       optionId: '0'
+  //     }, {
+  //       optionDes: '选项1',
+  //       optionId: '1'
+  //     }],
+  //     itemGrade: '1', //级别
+  //   }, {
+  //     itemType: 'text', //类型
+  //     itemCode: '', //编码
+  //     itemDesc: '问题三', //名称
+  //     isRequired: true, //是否必填
+  //     itemGrade: '1', //级别
+  //   }, {
+  //     itemType: 'rateSingle', //类型
+  //     itemCode: '', //编码
+  //     itemDesc: '问题四', //名称
+  //     isRequired: false, //是否必填
+  //     options: [{ //描述
+  //       optionDes: '选项0',
+  //       optionId: '0',
+  //       rateNum: 1
+  //     }, {
+  //       optionDes: '选项1',
+  //       optionId: '1',
+  //       rateNum: 2
+  //     }],
+  //     itemGrade: '1', //级别
+  //   },
+  //   {
+  //     itemType: 'rateMulti', //类型
+  //     itemCode: '', //编码
+  //     itemDesc: '问题五', //名称
+  //     isRequired: false, //是否必填
+  //     options: [{ //描述
+  //       optionDes: '选项0',
+  //       optionId: '0',
+  //       rateNum: 1
+  //     }, {
+  //       optionDes: '选项1',
+  //       optionId: '1',
+  //       rateNum: 2
+  //     }],
+  //     itemGrade: '1', //级别
+  //   }]  // 问卷列表数据
+  // };
   // 问卷类型映射关系
   var questItemMap = {
     radio: {
@@ -149,37 +229,37 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
   // 渲染创建问卷
   function renderStepPage2 () {
     var getTpl = questBodyHtml.innerHTML, wrap = document.getElementById('questBodyWrap');
-    laytpl(getTpl).render(step2Data, function (html) {
+    laytpl(getTpl).render(questPageData, function (html) {
       wrap.innerHTML = html;
       form.render()
     });
   }
   window.getQuestTitleInput = function (obj) {
-    step2Data.questTitle = $(obj).val()
+    questPageData.questTitle = $(obj).val()
     renderStepPage2()
   }
   // 获取题型标题
   window.getTitleInput = function (obj, index) {
-    step2Data.questList[index].itemDesc = $(obj).val()
+    questPageData.questList[index].itemDesc = $(obj).val()
     renderStepPage2()
     renderOutLine()
   }
   //题型选择
   window.addQuestItem = function (type) {
-    step2Data.questList.push(questItemMap[type])
+    questPageData.questList.push(questItemMap[type])
     renderStepPage2()
   }
   // 删除题目
   window.delItem = function (itemIndex) {
-    step2Data.questList.splice(itemIndex, 1)
+    questPageData.questList.splice(itemIndex, 1)
     renderStepPage2()
   }
   // 上下移动题目
   window.exchangeItem = function (index1, index2) {
-    if (index1 > -1 && index2 > -1 && index2 < step2Data.questList.length) {
-      var temp = step2Data.questList[index1];
-      step2Data.questList[index1] = step2Data.questList[index2];
-      step2Data.questList[index2] = temp;
+    if (index1 > -1 && index2 > -1 && index2 < questPageData.questList.length) {
+      var temp = questPageData.questList[index1];
+      questPageData.questList[index1] = questPageData.questList[index2];
+      questPageData.questList[index2] = temp;
       renderStepPage2()
     } else {
       layer.msg('不可移动了')
@@ -187,7 +267,7 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
   }
   // 获取选项标题
   window.getOptionInput = function (obj, index, optionIndex) {
-    step2Data.questList[index].options[optionIndex].optionDes = $(obj).val()
+    questPageData.questList[index].options[optionIndex].optionDes = $(obj).val()
     renderStepPage2()
   }
   // 添加选项
@@ -196,23 +276,23 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
       optionDes: "选项",
       optionId: ""
     }
-    if (['rateSingle', 'rateMulti'].indexOf(step2Data.questList[itemIndex].itemType) > -1) {
+    if (['rateSingle', 'rateMulti'].indexOf(questPageData.questList[itemIndex].itemType) > -1) {
       option.rateNum = 0;
     }
-    step2Data.questList[itemIndex].options.push(option)
+    questPageData.questList[itemIndex].options.push(option)
     renderStepPage2()
   }
   // 删除选项
   window.delOption = function (itemIndex, optionIndex) {
-    step2Data.questList[itemIndex].options.splice(optionIndex, 1)
+    questPageData.questList[itemIndex].options.splice(optionIndex, 1)
     renderStepPage2()
   }
   // 上下移动选项
   window.exchangeOption = function (itemIndex, index1, index2) {
-    if (index1 > -1 && index2 > -1 && index2 < step2Data.questList[itemIndex].options.length) {
-      var temp = step2Data.questList[itemIndex].options[index1];
-      step2Data.questList[itemIndex].options[index1] = step2Data.questList[itemIndex].options[index2];
-      step2Data.questList[itemIndex].options[index2] = temp;
+    if (index1 > -1 && index2 > -1 && index2 < questPageData.questList[itemIndex].options.length) {
+      var temp = questPageData.questList[itemIndex].options[index1];
+      questPageData.questList[itemIndex].options[index1] = questPageData.questList[itemIndex].options[index2];
+      questPageData.questList[itemIndex].options[index2] = temp;
       renderStepPage2()
     } else {
       layer.msg('不可移动了')
@@ -220,24 +300,24 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
   }
   // 评分项获取分值设置
   window.getRateInput = function (obj, index, optionIndex) {
-    step2Data.questList[index].options[optionIndex].rateNum = parseInt($(obj).val())
+    questPageData.questList[index].options[optionIndex].rateNum = parseInt($(obj).val())
     renderStepPage2()
   }
   // 必答选项交互渲染
   window.changeRequired = function (obj, index) {
-    step2Data.questList[index].isRequired = $(obj).prop("checked")
+    questPageData.questList[index].isRequired = $(obj).prop("checked")
     renderStepPage2()
   }
   // 渲染问题大纲
-  function renderOutLine(){
+  function renderOutLine () {
     var titles = []
-      step2Data.questList.forEach(function (item) {
-        titles.push(item.itemDesc)
-      })
-      var getTpl = outlineHtml.innerHTML, wrap = document.getElementById('outlineWrap');
-      laytpl(getTpl).render(titles, function (html) {
-        wrap.innerHTML = html;
-      });
+    questPageData.questList.forEach(function (item) {
+      titles.push(item.itemDesc)
+    })
+    var getTpl = outlineHtml.innerHTML, wrap = document.getElementById('outlineWrap');
+    laytpl(getTpl).render(titles, function (html) {
+      wrap.innerHTML = html;
+    });
   }
   element.on('tab(tabBrief)', function (obj) {
     //问题大纲
@@ -246,9 +326,36 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
     }
   })
   window.nextPage2 = function () {
-    console.log('第二步数据===', step2Data)
-    // element.tabChange('pageTab', '33');
+    console.log('第二步数据===', questPageData)
+    element.tabChange('pageTab', '33');
   }
+  /******************************************************* 问卷预览 *******************************************************/
+  function renderComputer () {
+    var getTpl = computerHtml.innerHTML, wrap = document.getElementById('computerWrap');
+    laytpl(getTpl).render(questPageData, function (html) {
+      wrap.innerHTML = html;
+      form.render()
+    });
+  }
+  function renderPhone () {
+    var getTpl = phoneHtml.innerHTML, wrap = document.getElementById('phoneWrap');
+    laytpl(getTpl).render(questPageData, function (html) {
+      wrap.innerHTML = html;
+      form.render()
+    });
+  }
+  window.nextPage3 = function () {
+    element.tabChange('pageTab', '44');
+  }
+  element.on('tab(tabPhoneComputer)', function (obj) {
+    if (obj.index == 0) {
+      renderPhone()
+    } else if (obj.index == 1) {
+      renderComputer()
+    }
+  });
+  element.tabChange('tabPhoneComputer', '11');
+  renderPhone()
   /******************************************************* tab操作 *******************************************************/
   element.on('tab(pageTab)', function (obj) {
     if (obj.index == 0) {
@@ -258,7 +365,7 @@ layui.use(['form', 'laytpl', 'jquery', 'table', 'element', 'layer'], function ()
     }
   });
   //默认打开tab
-  element.tabChange('pageTab', '11');
+  element.tabChange('pageTab', '33');
 
 
 });
